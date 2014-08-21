c956a8ee1beef0a1b54884619c0d3667e6f58960
sowdust <sowdust@gmail.com>
2014-08-21 23:22:54 +0200

    estensioni makefile

--- C/esercizio6/analisi.c
+++ C/esercizio6/analisi.c
@@ -112,36 +112,3 @@ void main(int argc, char* argv[])
 	
 	fclose(ouput_file);
 }
-
-
-void quickSort( int a[], int l, int r)
-{
-   int j;
-
-   if( l < r ) 
-   {
-   	// divide and conquer
-        j = partition( a, l, r);
-       quickSort( a, l, j-1);
-       quickSort( a, j+1, r);
-   }
-	
-}
-
-
-
-int partition( int a[], int l, int r) {
-   int pivot, i, j, t;
-   pivot = a[l];
-   i = l; j = r+1;
-		
-   while( 1)
-   {
-   	do ++i; while( a[i] <= pivot && i <= r );
-   	do --j; while( a[j] > pivot );
-   	if( i >= j ) break;
-   	t = a[i]; a[i] = a[j]; a[j] = t;
-   }
-   t = a[l]; a[l] = a[j]; a[j] = t;
-   return j;
-}
