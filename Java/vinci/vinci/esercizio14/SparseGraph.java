eddf1da9f09c647d84f7387f26d76ab34818b0e8
sowdust <sowdust@gmail.com>
2014-08-27 18:02:41 +0200

    sparse graph da rifare

--- Java/vinci/vinci/esercizio14/SparseGraph.java
+++ Java/vinci/vinci/esercizio14/SparseGraph.java
@@ -1,29 +1,59 @@
 package vinci.esercizio14;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * **
+ * UNO SCHIFO! CREARE CLASSI INTERNE PER RAPPRESENTARE NODI<V> E ARCHI<E>
+ *
+ * @author mat
+ * @param <V>
+ * @param <E>
+ */
 public class SparseGraph<V, E> implements Graph {
 
-    List<V> nodes;
-    List<V> adjacent;
-    List<E> incident;
+    private final List<List<Integer>> nodes;
+    //private final List<V> adjacent;
+    private final HashMap<V, Integer> position;
 
     SparseGraph() {
         nodes = new ArrayList<>();
-        adjacent = new LinkedList<>();
-        incident = new LinkedList<>();
+        position = new HashMap<>();
     }
 
     @Override
     public boolean addVertex(Object vertex) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Integer pos = position.get((V) vertex);
+        if (null != pos) {
+            return false;
+        }
+        //  add node list to vertices list
+        List<Integer> l = new LinkedList<>();
+        nodes.add(l);
+        //  add its position to hashtable
+        position.put((V) vertex, nodes.size() - 1);
+        return true;
     }
 
     @Override
     public boolean addEdge(Object v1, Object v2, Object info) throws IllegalArgumentException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Integer pv1 = position.get((V) v1);
+        Integer pv2 = position.get((V) v2);
+        if (pv1 == null || pv2 == null) {
+            throw new IllegalArgumentException();
+        }
+        List<Integer> adjacent = nodes.get(pv1);
+        for (Integer v : adjacent) {
+            //  if vertex (position) already in adjacency list
+            if (v.equals(pv2)) {
+                return false;
+            }
+        }
+        adjacent.add(pv2);
+        return true;
     }
 
     @Override
